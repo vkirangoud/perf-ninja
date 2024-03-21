940cd1b92d0b86da8197f65eb58863d8fe495bbc
vkirangoud <kirangoud.v@gmail.com>
2024-03-21 10:45:57 +0530

    O(1) solution is provided for sum of n numbers

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -2,9 +2,13 @@
 #include "solution.h"
 
 int solution(int *arr, int N) {
+
+#if 0
   int res = 0;
   for (int i = 0; i < N; i++) {
     res += arr[i];
   }
   return res;
+#endif
+  return (N * (N +1))/2 ;
 }
